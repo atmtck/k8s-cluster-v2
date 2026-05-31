460e95abe32c7152ab556dd02f8d0868fc275627
atmtc <riccardo@tuta.io>
2026-06-01 00:15:19 +0200

    update wireguard config generator to reflect new network config

--- wg/wg-gen.sh
+++ wg/wg-gen.sh
@@ -1,44 +1,54 @@
 #!/bin/sh
 
 hosts='
-hbox1.ic3.systems 10.90.0.1
-hbox2.ic3.systems 10.90.0.2
-hbox3.ic3.systems 10.90.0.3
+hbox1.atmtc.eu 10.90.0.1/28 10.244.1.0/24
+hbox2.atmtc.eu 10.90.0.2/28 10.244.2.0/24
+hbox3.atmtc.eu 10.90.0.3/28 10.244.3.0/24
 '
-
+port='51820'
+mtu='1412'
 tmp_file="tmp.txt"
+
+presharedkey=$( wg genkey )
 
 printf "%s" "$hosts" | tail -n +2 | while IFS='' read line;do
     hostname=$( echo "$line" | cut -d ' ' -f 1 )
     ip=$( echo "$line" | cut -d ' ' -f 2 )
     privkey=$( wg genkey )
     pubkey=$( echo $privkey | wg pubkey )
-    echo "$hostname $ip $privkey $pubkey" >> $tmp_file
+    echo "$line $privkey $pubkey" >> $tmp_file
 done
 
 cat $tmp_file | while read host; do
-    outfile="$( echo "$host" | cut -d '.' -f 1 ).conf"
-    ip=$( echo "$host" | cut -d ' ' -f 2 )
-    privkey=$( echo "$host" | cut -d ' ' -f 3 )
+    outdir="$( echo "$host" | cut -d '.' -f 1 )"
+    outfile="$outdir/wg0.conf"
+    ip=$( echo "$host" | cut -d ' ' -f 2 | cut -d '/' -f 1 )
+    mask=$( echo "$host" | cut -d ' ' -f 2 | cut -d '/' -f 2 )
+    privkey=$( echo "$host" | cut -d ' ' -f 4 )
+
+    mkdir -p $outdir
 
     > $outfile
     echo "[Interface]" >> $outfile
     echo "PrivateKey = $privkey" >> $outfile
-    echo "Address = $ip/24" >> $outfile
-    echo "MTU = 1412" >> $outfile
-    echo "ListenPort = 51820" >> $outfile
+    echo "Address = $ip/$mask" >> $outfile
+    echo "MTU = $mtu" >> $outfile
+    echo "ListenPort = $port" >> $outfile
+    echo "Table = off" >> $outfile
 
     cat $tmp_file | while read peer_host; do
         if [ "$host" != "$peer_host" ]; then
             hostname=$( echo "$peer_host" | cut -d ' ' -f 1 )
-            ip=$( echo "$peer_host" | cut -d ' ' -f 2 )
-            pubkey=$( echo "$peer_host" | cut -d ' ' -f 4 )
+            ip=$( echo "$peer_host" | cut -d ' ' -f 2 | cut -d '/' -f 1 )
+            pod_network=$( echo "$peer_host" | cut -d ' ' -f 3)
+            pubkey=$( echo "$peer_host" | cut -d ' ' -f 5 )
 
             echo "" >> $outfile
             echo "[Peer]" >> $outfile
-            echo "Endpoint = $hostname:51820" >> $outfile
+            echo "Endpoint = $hostname:$port" >> $outfile
             echo "PublicKey = $pubkey" >> $outfile
-            echo "AllowedIPs = $ip/32" >> $outfile
+            echo "PresharedKey = $presharedkey" >> $outfile
+            echo "AllowedIPs = $ip/32,$pod_network" >> $outfile
         fi
     done
 done
